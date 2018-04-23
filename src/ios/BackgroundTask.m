713c97f175f7d3861892700a107e48be62129157
achubutkin <a.chubutkin@gmail.com>
2018-04-23 14:20:56 +0600

    Init

--- src/ios/BackgroundTask.m
+++ src/ios/BackgroundTask.m
@@ -3,6 +3,7 @@
 //  Copyright (c) 2014 Lee Crossley - http://ilee.co.uk
 //  Executing Finite-Length Tasks
 
+
 #import "Cordova/CDV.h"
 #import "Cordova/CDVViewController.h"
 #import "BackgroundTask.h"
